5b2adb9fe45c8510a7fc308fc53d74396d97f4e5
Abhishek Mali <abhishekrockstar545@gmail.com>
2023-04-18 22:10:15 +0530

    exposing the service class to controllers

--- blogging-application/src/main/java/com/blog/controllers/PostController.java
+++ blogging-application/src/main/java/com/blog/controllers/PostController.java
@@ -1,8 +1,11 @@
 package com.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +26,19 @@ public class PostController {
 	public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO, @PathVariable Integer userId,
 			@PathVariable Integer categoryId) {
 		PostDTO post = this.postService.createPost(postDTO, userId, categoryId);
-		return new ResponseEntity<>(post, HttpStatus.CREATED);
+		return new ResponseEntity<PostDTO>(post, HttpStatus.CREATED);
 	}
+
+	@GetMapping("/category/{categoryId}/posts")
+	public ResponseEntity<List<PostDTO>> getPostsByCategory(@PathVariable Integer categoryId) {
+		List<PostDTO> posts = this.postService.getPostsByCategory(categoryId);
+		return new ResponseEntity<List<PostDTO>>(posts, HttpStatus.OK);
+	}
+
+	@GetMapping("/user/{userId}/posts")
+	public ResponseEntity<List<PostDTO>> getPostsByUser(@PathVariable Integer userId) {
+		List<PostDTO> posts = this.postService.getPostsByUser(userId);
+		return new ResponseEntity<List<PostDTO>>(posts, HttpStatus.OK);
+	}
+
 }
