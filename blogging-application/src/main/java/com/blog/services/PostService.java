2c3296230c38aabc0b8c54342afbaea342c2869a
Abhishek Mali <abhishekrockstar545@gmail.com>
2023-04-18 10:38:57 +0530

    added PostService interface

---
+++ blogging-application/src/main/java/com/blog/services/PostService.java
@@ -0,0 +1,32 @@
+package com.blog.services;
+
+import java.util.List;
+
+import com.blog.payloads.PostDTO;
+
+public interface PostService {
+
+	// create
+	PostDTO createPost(PostDTO postDTO, Integer userId, Integer categoryId);
+
+	// update
+	PostDTO updatePost(PostDTO postDTO, Integer postId);
+
+	// delete
+	void deletePost(Integer postId);
+
+	// get single post
+	PostDTO getPostById(Integer postId);
+
+	// get all posts
+	List<PostDTO> getAllPosts();
+
+	// get all posts by category
+	List<PostDTO> getPostsByCategory(Integer categoryId);
+
+	// get all posts by user
+	List<PostDTO> getPostsByUser(Integer userId);
+
+	// search posts
+	List<PostDTO> searchPosts(String keyword);
+}
