99b65374a1b38858544bf29ac6052dfc70d49af8
Abhishek Mali <abhishekrockstar545@gmail.com>
2023-04-18 10:32:57 +0530

    added lombox annotations to Post.java

--- blogging-application/src/main/java/com/blog/entities/Post.java
+++ blogging-application/src/main/java/com/blog/entities/Post.java
@@ -1,26 +1,41 @@
 package com.blog.entities;
 
-import javax.persistence.*;
 import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "posts")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Post {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer postId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer postId;
 
-    @Column(name = "title", nullable = false)
-    private String title;
-    @Column(length = 10024)
-    private String content;
-    private String imageName;
-    private Date addedDate;
+	@Column(name = "title", nullable = false)
+	private String title;
+	@Column(length = 10024)
+	private String content;
+	private String imageName;
+	private Date addedDate;
 
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
-    @ManyToOne
-    private User user;
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+	@ManyToOne
+	private User user;
 }
