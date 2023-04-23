7f83d05ff1aad4b4352906ba53f08fa24d08e8a8
Abhishek Mali <abhishekrockstar545@gmail.com>
2023-04-23 15:40:43 +0530

    sql query for role and user_role

--- blog.sql
+++ blog.sql
@@ -2,3 +2,5 @@ SELECT * FROM blogging_application.users;
 SELECT * FROM blogging_application.categories;
 SELECT * FROM blogging_application.posts;
 SELECT * FROM blogging_application.comments;
+SELECT * FROM blogging_application.role;
+SELECT * FROM blogging_application.user_role;
