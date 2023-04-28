6bccf7b42d698369f1926d0ef6da7164cfc0748f
Abhishek Mali <abhishekrockstar545@gmail.com>
2023-04-28 11:29:07 +0530

    EnableGlobalMethodSecurity to access delete api only to admin role

--- blogging-application/src/main/java/com/blog/config/SpringSecurityConfig.java
+++ blogging-application/src/main/java/com/blog/config/SpringSecurityConfig.java
@@ -3,8 +3,10 @@ package com.blog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +22,7 @@ import com.blog.security.JwtAuthenticationFilter;
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -44,6 +47,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		.disable()
 		.authorizeHttpRequests()
 		.antMatchers("/api/auth/login").permitAll()
+		// .antMatchers(HttpMethod.GET).permitAll() // enable get requests without authorization
 		.anyRequest()
 		.authenticated()
 		.and()
