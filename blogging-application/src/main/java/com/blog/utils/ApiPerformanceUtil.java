35aabb1d1e574aec23e82eaedf63b714dd1e1b59
Abhishek Mali <abhishekrockstar545@gmail.com>
2023-04-29 23:25:58 +0530

    added performance util file to see the time of api

---
+++ blogging-application/src/main/java/com/blog/utils/ApiPerformanceUtil.java
@@ -0,0 +1,17 @@
+package com.blog.utils;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class ApiPerformanceUtil {
+	public static void measure(Runnable runnable, String apiName) {
+		long startTime = System.currentTimeMillis();
+		try {
+			runnable.run();
+		} finally {
+			long endTime = System.currentTimeMillis();
+			long elapsedTime = endTime - startTime;
+			log.info(apiName + " took " + elapsedTime + " ms to complete");
+		}
+	}
+}
